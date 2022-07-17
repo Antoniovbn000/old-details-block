3aeade68900d5a4f8bb11aafd62ad13f8f1b4bfd
Antoniovbn000 <63794922+Antoniovbn000@users.noreply.github.com>
2022-07-18 00:17:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-echo "Installing"
-sleep 2
-git clone https://github.com/Antoniovbn000/old-details-block.git
-cd old-details-block
-mv -r resources /var/www/pterodactyl
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt install -y nodejs
-npm i -g yarn
-cd /var/www/pterodactyl
-yarn
-cd /var/www/pterodactyl
-yarn build:production
+git clone https://github.com/Antoniovbn000/old-details-block.git && cd old-details-block && cp -r resources /var/www/pterodactyl && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt install -y nodejs && npm i -g yarn && cd /var/www/pterodactyl && yarn && cd /var/www/pterodactyl && yarn build
